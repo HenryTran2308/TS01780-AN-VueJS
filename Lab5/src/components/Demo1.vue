c2b9060000d505181b151389e423b78a89315566
Henry Tran <henry790205@gmail.com>
2026-01-26 17:41:18 +0700

    Nop Lab5

---
+++ Lab5/src/components/Demo1.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <h1>FPT PolyTechnic</h1>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+<script setup>
+defineProps({
+    msg: String
+})
+</script>
+<style lang="">
+    
+</style>
